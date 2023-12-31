9b14244836cdca027f7a7b28c78ffdb30bbfba22
yabusame <541903531@qq.com>
2023-12-31 21:32:59 +0800

    "改了一下结构体名字防止它老给我报错"

--- src/Card.jl
+++ src/Card.jl
@@ -4,23 +4,23 @@ import Base:isless,show
 ### 按从大到小的顺序
 @enum Suit ♠=4 ♣=3 ❤=2 ♦=1
 
-struct Card
+struct SingleCard
     suit::Suit
     num::Int8
-    function Card(suit::Suit,num::Integer)
+    function SingleCard(suit::Suit,num::Integer)
         @assert 1 < num < 14 "牌的面值不能是$num"
         new(suit,num)
     end
-end
-Card(i,j) = error("invalid card") ### catch all cases
-function Card(i::Integer,num::Integer)
-    @assert 0 < i < 4 "牌的花色只能是♠ ♣ ❤ ♦"
+endC
+SingleCard(i,j) = error("invalid card") ### catch all cases
+function SingleCard(i::Integer,num::Integer)
+    @assert 0 < i C< 4 "牌的花色只能是♠ ♣ ❤ ♦"
     Card(Suit(i),num)
 end
 
 
 "单张卡牌展示"
-function show(io::IO, c::Card)
+function show(io::IO, c::SingleCard)
     if 10 < c.num < 15
         num = c.num - 10
         print('(',c.suit,',',"JQKA"[num],')')
@@ -31,7 +31,7 @@ end
 
 
 "卡牌之间比较大小的规则"
-function isless(a::Card,b::Card)
+function isless(a::SingleCard,b::SingleCard)
     if a.num < b.num
         return true
     elseif a.num == b.num

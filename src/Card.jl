b0d1e3c66fdb81a264e46db4f5d287a8c86e3669
yabusame <541903531@qq.com>
2023-12-31 21:23:14 +0800

    “写了一下普通的扑克牌和他们的大小比较”

--- src/Card.jl
+++ src/Card.jl
@@ -1,5 +1,47 @@
 module Card
-
 # Happy new year
+import Base:isless,show
+### 按从大到小的顺序
+@enum Suit ♠=4 ♣=3 ❤=2 ♦=1
 
+struct Card
+    suit::Suit
+    num::Int8
+    function Card(suit::Suit,num::Integer)
+        @assert 1 < num < 14 "牌的面值不能是$num"
+        new(suit,num)
+    end
 end
+Card(i,j) = error("invalid card") ### catch all cases
+function Card(i::Integer,num::Integer)
+    @assert 0 < i < 4 "牌的花色只能是♠ ♣ ❤ ♦"
+    Card(Suit(i),num)
+end
+
+
+"单张卡牌展示"
+function show(io::IO, c::Card)
+    if 10 < c.num < 15
+        num = c.num - 10
+        print('(',c.suit,',',"JQKA"[num],')')
+    else
+        print('(',c.suit,',',c.num,')')
+    end
+end
+
+
+"卡牌之间比较大小的规则"
+function isless(a::Card,b::Card)
+    if a.num < b.num
+        return true
+    elseif a.num == b.num
+        if a.suit < b.suit
+            return true
+        end
+    end
+    return false
+end
+
+
+
+end # Card

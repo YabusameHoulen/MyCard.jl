0bc9129749e7ef592c7935be12bab3f27db034bb
yabusame <541903531@qq.com>
2024-01-06 14:47:24 +0800

    我改！

--- src/MyCard.jl
+++ src/MyCard.jl
@@ -1,25 +1,30 @@
 module MyCard
 # Happy new year
-import Base: isless, show
+import Base: isless, show, (:-)
+import Base: convert, zero, (:+)
+import Base: rem
 
 export SingleCard
-export Suit, ♠, ♣, ❤, ♦
+export Suit, ♠, ♣, ♡, ♢
 
 ### 按从大到小的顺序
-@enum Suit ♠ = 4 ♣ = 3 ❤ = 2 ♦ = 1
-
+@enum Suit ♢ ♡ ♣ ♠
+@enum King 🐍 🐉
 
 struct SingleCard
-	suit::Suit
+	suit::Union{Suit,King}
 	num::Int8
 	function SingleCard(suit::Suit, num::Integer)
 		1 < num < 14 || throw(DomainError("牌的面值不能是$num"))
 		new(suit, num)
 	end
+	function SingleCard(suit::King)
+		suit == King(0) ? new(King(0),98) : new(King(1),99)
+	end
 end
 SingleCard(i, j) = throw(ArgumentError("Invalid Card")) ### catch all cases
 function SingleCard(i::Integer, num::Integer)
-	@assert 0 < i C < 4 "牌的花色只能是♠ ♣ ❤ ♦"
+	i in 0:3 || throw(ArgumentError("牌的花色只能是♠ ♣ ❤ ♦"))
 	SingleCard(Suit(i), num)
 end
 
@@ -27,7 +32,7 @@ end
 "单张卡牌展示"
 function show(io::IO, c::SingleCard)
 	big_num = Symbol[:J, :Q, :K, :A]
-	if 10 < c.num < 15
+	if c.num in 11:14
 		num = c.num - 10
 		show(io, (c.suit, big_num[num]))
 	else
@@ -35,6 +40,8 @@ function show(io::IO, c::SingleCard)
 	end
 end
 
+"Suit转换规则"
+convert(::Type{Suit}, x::Int) = Suit(x)
 
 "卡牌之间比较大小的规则"
 function isless(a::SingleCard, b::SingleCard)
@@ -48,7 +55,8 @@ function isless(a::SingleCard, b::SingleCard)
 	return false
 end
 
-
+"可以定义一些奇怪的Suit的加减法"
+-(a::Suit, b::Suit) = (suit = Int(a) - Int(b); suit < 1 ? Suit(1) : Suit(suit))
++(a::Suit, b::Suit) = (suit = Int(a) + Int(b); suit > 4 ? Suit(4) : Suit(suit))
 
 end # MyCard
-
